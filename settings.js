d5cdf8df814b360dde5054680ddb6b2ba6182c73
SkibidiSalesman <humidor_easel.0a@icloud.com>
2025-02-08 23:02:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "LTest",
   botOwner: 'LuisenAlt', // Your name
-  ownerNumber: '+491713310251', //Your number
+  ownerNumber: '+4917672415053', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
